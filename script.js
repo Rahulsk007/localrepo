58ee68a02c4a8fd4aadc8df291834b131a2d604c
Rahulsk007 <rsk190102@gmail.com>
2024-06-23 19:00:13 +0530

    add dark and Light Mode

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const body = document.querySelector('body');
 n.addEventListener('click', () =>{
     body.style.backgroundColor = 'black';
 });
+
+const color = () => {
+    body.style.backgroundColor = 'black';
+
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click' , () => {
+    if (body.style.backgroundColor == "white"){
+
+        body.style.backgroundColor = "black"
+    }else{
+        body.style.backgroundColor = "white";
+    }
+
+})
